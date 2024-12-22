c3e31e8fbd48420e745f142a50c1bc0902d413fc
AchmadFarudinAzam <107410730+AchmadFarudinAzam@users.noreply.github.com>
2024-12-23 00:21:58 +0700

    script.js

--- script.js
+++ script.js
@@ -71,10 +71,11 @@ function showQRCode(modelURL) {
     });
     if (model) {
       document.getElementById('modelViewer').src = `assest/models/${model}`;
+        qrModal.style.display = "flex";
     } else {
       document.body.innerHTML = "<h1>Model Not Found!</h1>"
-    };
-    qrModal.style.display = "flex";
+    }
+    
 }
 document.querySelectorAll('.ar-button').forEach(button => {
     button.addEventListener('click', () => {
