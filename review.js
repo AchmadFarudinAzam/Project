3bb55e4d37cf74f10fce8ee3f41523f25056fc4e
AchmadFarudinAzam <107410730+AchmadFarudinAzam@users.noreply.github.com>
2024-12-26 10:39:40 +0700

    review.js

--- review.js
+++ review.js
@@ -1,4 +1,5 @@
 document.getElementById('submitBtn').addEventListener('click', function () {
+  console.log('button clik')
   // Ambil nilai dari formulir
   const name = document.getElementById('name').value;
   const model = document.getElementById('model').value;
